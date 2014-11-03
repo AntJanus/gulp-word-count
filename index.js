6f06efca12bcc57976954218e08f8bec7adeac91
Antonin Januska <antonin@antjanus.com>
2014-11-02 22:41:52 -0500

    WC prototype

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-var through = require('through2');
-var gutil = require('gulp-util');
+var through     = require('through2');
+var gutil       = require('gulp-util');
+var wc          = require('wordcount');
 var PluginError = gutil.PluginError;
 
 module.export = function(file, opt) {
   var total = 0;
+  var firstFile;
 
 	function bufferContents(file, enc, cb) {
     var wordCount = 0;
 
     if(file.isBuffer()) {
+      if (!firstFile) firstFile = file;
 
+      wordcount = wc(string(file.contents));
 
       total += wordCount;
     }
 	}
 
 	function endStream() {
-    console.log(total);
 
-    this.emit('data', total);
+     if (firstFile) {
+      var joinedFile = firstFile;
+
+      if (typeof file === 'string') {
+        joinedFile = firstFile.clone({contents: false});
+        joinedFile.path = path.join(firstFile.base, file);
+      }
+
+      joinedFile.contents = new Buffer(total);
+
+      this.emit('data', joinedFile);
+    }
+
+   console.log(total);
+
 		this.emit('end');
 	}
 
